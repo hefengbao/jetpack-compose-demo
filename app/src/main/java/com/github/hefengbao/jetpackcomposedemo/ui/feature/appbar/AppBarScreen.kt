3447b098197ff848ef927edba672842274904ee3
hefengbao <754582231@qq.com>
2023-08-21 15:27:27 +0800

    update

---
+++ app/src/main/java/com/github/hefengbao/jetpackcomposedemo/ui/feature/appbar/AppBarScreen.kt
@@ -0,0 +1,105 @@
+package com.github.hefengbao.jetpackcomposedemo.ui.feature.appbar
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBarScreen(
+    modifier: Modifier = Modifier
+) {
+    Column (
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        TopAppBar(
+            title = { 
+                Text(text = "8ug.icu")
+            },
+            navigationIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Home, contentDescription = null)
+                }
+            },
+            actions = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Share, contentDescription = null)
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Settings, contentDescription = null)
+                }
+            }
+        )
+        
+        MediumTopAppBar(
+            title = {
+                Text(text = "8ug.icu")
+            }
+        )
+
+        LargeTopAppBar(
+            title = {
+                Text(text = "8ug.icu")
+            }
+        )
+
+        CenterAlignedTopAppBar(
+            title = {
+                Text(text = "8ug.icu")
+            }
+        )
+
+        BottomAppBar {
+            OutlinedTextField(value = "", onValueChange = {})
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "提交")
+            }
+        }
+
+        BottomAppBar(
+            actions = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Share, contentDescription = null)
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.ThumbUp, contentDescription = null)
+                }
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Edit, contentDescription = null)
+                }
+            }
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun TopAppBarScreenPreview() {
+    AppBarScreen()
+}
---
+++ app/src/main/java/com/github/hefengbao/jetpackcomposedemo/ui/feature/radiobutton_checkbox_switch/RadioSwitchScreen.kt
@@ -0,0 +1,79 @@
+package com.github.hefengbao.jetpackcomposedemo.ui.feature.radiobutton_checkbox_switch
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RadioButtonCheckBoxSwitchScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        val radioList = listOf<String>("苹果","芒果","结果")
+        var radioSelected: Int? by remember { mutableStateOf(null) }
+        val radioSelectedText = if (radioSelected != null){
+            "(${radioList[radioSelected!!]})"
+        }else{
+            "()"
+        }
+
+        Text(text = "那个不是水果？${radioSelectedText}")
+
+        radioList.forEachIndexed { index,item ->
+            Row(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .clickable { radioSelected = index },
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                RadioButton(selected = index == radioSelected, onClick = { radioSelected = index })
+                Text(text = item)
+            }
+        }
+
+        var checkboxChecked by remember { mutableStateOf(false) }
+
+        Row(
+            modifier = modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Checkbox(checked = checkboxChecked, onCheckedChange = { checkboxChecked = it})
+            Text(text = "请阅读并同意《服务协议》")
+        }
+
+        var switchChecked by remember { mutableStateOf(false) }
+        Row(
+            modifier = modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Text(text = "开启消息通知")
+            Switch(checked = switchChecked, onCheckedChange = { switchChecked = it })
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun RadioButtonCheckBoxSwitchScreenPreview() {
+    RadioButtonCheckBoxSwitchScreen()
+}
+
